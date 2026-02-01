d0bc6b85143b8fa179340d4eda34c1b30ed02822
destinysimms <38442548+dnsimms@users.noreply.github.com>
2026-01-31 23:20:10 -0500

    I changed the timer to use ScheduledExecutorService and ScheduledFuture and reworked how it launched. Add a visual component for the terminal

--- src/main/java/destiny/creates/Egg.java
+++ src/main/java/destiny/creates/Egg.java
@@ -1,78 +1,107 @@
 package destiny.creates;
-import java.util.Timer;
-import java.util.TimerTask;
+import java.util.concurrent.*;
+import java.util.concurrent.TimeUnit.*;
 
 public class Egg {
 
+    //TO-DO: change the time to seconds and increase by one
     public enum EGG {
         FRIED{
             public String getEggType(){
                 return "FRIED";
             }
+            public long getCookTime() {return 11L;} //30s for testing purposes
         },
         HARD_BOILED{
             public String getEggType(){
                 return "HARD-BOILED";
             }
+            public long getCookTime() {return 30000L;} //30s for testing purposes
         },
         SOFT_BOILED{
             public String getEggType(){
                 return "SOFT-BOILED";
             }
+            public long getCookTime() {return 30000L;} //30s for testing purposes
         },
         SEMI_HARD_BOILED{
             public String getEggType(){
                 return "SEMI HARD-BOILED";
             }
+            public long getCookTime() {return 30000L;} //30s for testing purposes
         };
 
+        /**
+         * This method will return the value of the current EGG enum type.
+         * @return eggType as a string
+         */
         public abstract String getEggType();
+
+        /**
+         * This method will return the cook time for the specific EGG enum type.
+         * @return eggType cook time
+         */
+        public abstract long getCookTime();
     }//ENUM
 
     private final EGG eggType;
+    private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
 
-
+    /**
+     * Default constructor that sets the chosen eggType to the private global variable.
+     * @param eggType
+     */
     public Egg(EGG eggType){
         this.eggType = eggType;
     }
 
+    /**
+     * This method will print the value of the EGG enum type to the user.
+     */
     public void printEggType(){
         System.out.println(this.eggType.getEggType());
     }
 
+    /**
+     * This method will start the timer based on the current eggType. A message will print to the user, regardless of
+     * eggType, stating that their egg is done.
+     * @param eggType
+     */
     public void startTimer(EGG eggType){
-        Timer timer = new Timer();
-        TimerTask task = new TimerTask() {
+        final Runnable eggMessage = new Runnable() {
             @Override
             public void run() {
-                switch (eggType){
-                    case EGG.FRIED:
-                        System.out.println("Egg successfully fried!");
-                        break;
-                    case EGG.HARD_BOILED:
-                        System.out.println("Egg successfully hard-boiled!");
-                        break;
-                    case EGG.SOFT_BOILED:
-                        System.out.println("Egg successfully soft-boiled!");
-                        break;
-                    case EGG.SEMI_HARD_BOILED:
-                        System.out.println("Egg successfully semi-hard boiled!");
-                        break;
-                    default:
-                        break;
-                }
-                System.out.println("Finished egg timer");
-                timer.cancel();
+                System.out.println("\nYour egg is done!");
+                scheduler.shutdown();
             }
-        };//timer
+        };
+
+        final ScheduledFuture<?> eggTimerHandle = scheduler.schedule(eggMessage, eggType.getCookTime(), TimeUnit.SECONDS);
+        try{
+            visualCount(eggTimerHandle);
+        } catch (ExecutionException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
 
 
-        long delay = 30000L;
-        System.out.println("Starting egg timer");
-        timer.schedule(task,delay);
+    }
+    private void visualCount(ScheduledFuture<?> eggTimerHandle) throws ExecutionException, InterruptedException {
+        long startTime = System.currentTimeMillis();
+        while(!eggTimerHandle.isDone()){
+            long elapsedTime = System.currentTimeMillis() - startTime;
+            long elapsedSeconds = elapsedTime/1000;
 
+            String seconds = Integer.toString((int) (elapsedSeconds % 60));
+            String minutes = Integer.toString((int) ((elapsedSeconds % 3600) / 60));
+            String hours = Integer.toString((int) (elapsedSeconds / 3600));
 
+            if (seconds.length() < 2) seconds = "0" + seconds;
+            if (minutes.length() < 2) minutes = "0" + minutes;
+            if (hours.length() < 2) hours = "0" + hours;
 
-
+            String timeDisplay = hours + ":" + minutes + ":" + seconds;
+            System.out.print("\r" + timeDisplay);
+            Thread.sleep(100);
+        }
     }
 }
